9834c2607a31420e507b084d9c6121989c126a60
Owonikoko Olaoluwa <owonikokoolaoluwa@gmail.com>
2022-12-20 04:10:58 +0100

    add a general not found page

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import NotFound from "@/views/NotFound.vue"
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: "/:pathMatch(.*)*", name: "not-found", component: NotFound },
     {
       path: "/",
       name: "home",
@@ -17,7 +18,6 @@ const router = createRouter({
 
       component: ContactView,
     },
-    { path: "*", name: "not-found", component: NotFound },
   ],
   scrollBehavior(to, _, savedPosition) {
     if (to.hash) {
